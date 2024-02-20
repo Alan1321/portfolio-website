1d1a30c93645d0cfe2a011f42ef8a673388df287
Alan Subedi <alansubedi@Alans-Laptop.local>
2024-02-19 23:50:28 -0600

    working on header, need animation when it drops down + centering fixes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,32 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './header.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
+
+    const [Toggle, setToggle] = useState(false)
+
   return (
-    <header className="header">
+    <header className={Toggle ? "header activeee" : "header"}>
         <div className="logo">
             <a href="">Smith</a>
+            <div className="menu_bar" onClick={()=>setToggle(!Toggle)}>
+                <MenuIcon />
+            </div>
         </div>
-        <ul>
-            <li>
-                <a href="">Home</a>
-            </li>
-            <li>
-                <a href="">About</a>
-            </li>
-            <li>
-                <a href="">Skills</a>
-            </li>
-            <li>
-                <a href="">Services</a>
-            </li>
-            <li>
-                <a href="">Portfolio</a>
-            </li>
-            <li>
-                <a href="">Contact</a>
-            </li>
-        </ul>
+        <div className="nav_menu">
+            <ul className={Toggle ? "nav_list_web activee" : "nav_list_web"}>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Skills</a>
+                </li>
+                <li>
+                    <a href="">Services</a>
+                </li>
+                <li>
+                    <a href="">Portfolio</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </div>
     </header>
   )
 }
